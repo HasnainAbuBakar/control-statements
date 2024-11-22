4aa649268bdb46a540ac062c5e46569281a88088
Hasnain AbuBakar <hasnainabubakar404@gmail.com>
2024-11-22 20:56:28 +0500

    Update Odd Or Even.cpp

--- Odd Or Even.cpp
+++ Odd Or Even.cpp
@@ -1,18 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-	int enter_Num;
-	
-	cout<<"Enter an integer = ";
-	cin>>enter_Num;
-	
-	if(enter_Num / 2 == 0)
-	{
-		cout<<"This is Even";
-	}
-	else
-	{
-		cout<<"This is Odd";
-	}
+    int enter_Num;
+
+    cout << "Enter an integer: ";
+    cin >> enter_Num;
+    
+    if (enter_Num % 2 == 0)
+    {
+        cout << "This is Even";
+    }
+    else
+    {
+        cout << "This is Odd";
+    }
 }
