f786a6d5c41631e62fee74aabc06b0a3d85e83d8
Hasnain AbuBakar <hasnainabubakar404@gmail.com>
2024-11-23 14:36:52 +0500

    Update Calculate the electricity bill by units.cpp

--- Calculate the electricity bill by units.cpp
+++ Calculate the electricity bill by units.cpp
@@ -3,23 +3,27 @@ using namespace std;
 int main()
 {
 	int units;
-	cout<<"Enter total units if bill = ";
+	cout<<"Enter total units of bill = ";
 	cin>>units;
 	
 	if(units<=100)
 	{
-		cout<<"The cast is $0.50 per unit.";
+		cout<<"The cast is $0.50 per unit."<<endl;
+		cout<<"Total Bill is  "<<units*0.50<<endl;
 	}
 	else if(units<=200)
 	{
-		cout<<"The cast is $0.75 per unit.";
+		cout<<"The cast is $0.75 per unit."<<endl;
+		cout<<"Total Bill is  "<<units*0.75<<endl;
 	}
 	else if(units<=300)
 	{
-		cout<<"The cast is $1.20 per unit.";
+		cout<<"The cast is $1.20 per unit."<<endl;
+		cout<<"Total Bill is  "<<units*1.20<<endl;
 	}
 	else if(units>300)
 	{
-		cout<<"The cast is $1.50 per unit.";
+		cout<<"The cast is $1.50 per unit."<<endl;
+		cout<<"Total Bill is  "<<units*1.50;
 	}
 }
